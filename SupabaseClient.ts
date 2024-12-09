b960dc84f4c042f979a312be439b2377a042fac5
lipzy13 <88047810+lipzy13@users.noreply.github.com>
2024-12-09 15:58:22 +0700

    editing client for supabase

--- SupabaseClient.ts
+++ SupabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types.ts";
+import "jsr:@std/dotenv/load";
+
+const supabaseUrl = Deno.env.get("SUPABASE_URL") as string;
+const supabaseAnonKey = Deno.env.get("SUPABASE_ANON_KEY") as string;
 
 export const supabaseClient = createClient<Database>(
-  Deno.env.get("SUPABASE_URL")!,
-  Deno.env.get("SUPABASE_ANON_KEY")!,
+  supabaseUrl!,
+  supabaseAnonKey!,
 );
